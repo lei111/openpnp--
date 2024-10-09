f5c7803079ae05da8908b61f9be1b14635bb2f59
lei111 <165718275@qq.com>
2024-10-09 16:01:35 +0800

    Add files via upload

---
+++ openpnp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+platform='unknown'
+unamestr=`uname`
+case "$unamestr" in
+	Linux)
+		platform='linux'
+		rootdir="$(dirname $(readlink -f $0))"
+	;;
+	Darwin)
+		platform='mac'
+		rootdir="$(cd $(dirname $0); pwd -P)"
+	;;
+esac
+
+case "$platform" in
+	mac)
+		java -Xdock:name=OpenPnP --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.desktop/java.awt=ALL-UNNAMED --add-opens=java.desktop/java.awt.color=ALL-UNNAMED -jar $rootdir/target/openpnp-gui-0.0.1-alpha-SNAPSHOT.jar
+	;;
+	linux)
+		java $1 --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.desktop/java.awt=ALL-UNNAMED --add-opens=java.desktop/java.awt.color=ALL-UNNAMED -jar $rootdir/target/openpnp-gui-0.0.1-alpha-SNAPSHOT.jar
+	;;
+esac
